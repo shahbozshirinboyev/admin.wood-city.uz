7340e104272dd366c90229fb04b26a9b17d01b7b
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2024-12-18 14:20:06 +0500

    Create product card

--- src/App.jsx
+++ src/App.jsx
@@ -511,14 +511,45 @@ function App() {
             </button>
           </div>
 
-          <div className="mt-6 grid grid-cols-3 gap-6">
-            {activeProducts?.map((product) => (
-              <div key={product.id} className="border p-2">
-                <div className="flex justify-between items-center mb-2">
-                  <span>{product?.title}</span>
-                  <span>{product?.price}</span>
-                  <span>{product?.fix_price}</span>
-                  <button
+          
+          <div className="grid grid-cols-2 md:grid-cols-3  xl:grid-cols-4 gap-4 my-4">
+          {/* Card Design START */}
+          {activeProducts?.map((product, index) => (
+            <div className="border p-4" key={index}>
+              <img src={product?.image_product} alt="" className="h-[220px] mx-auto" />
+
+              <div className="flex gap-4">
+                <p className="text-start line-clamp-2 font-semibold w-full">
+                  {product?.title}
+                </p>
+                <div className="text-end">
+                  <p className="whitespace-nowrap font-semibold">
+                    {product?.price}
+                  </p>
+                  <p className="whitespace-nowrap line-through text-[14px] font-semibold opacity-50">
+                    {product?.fix_price}
+                  </p>
+                </div>
+              </div>
+
+              <div className="py-4">
+                <p className="font-semibold">Габариты:</p>
+                <div className="grid grid-cols-3 text-start text-[14px] gap-2 py-1">
+                  <div>
+                    <p className="opacity-60">Длина</p> <p>{product?.length} см</p>
+                  </div>
+                  <div>
+                    <p className="opacity-60">Ширина</p> <p>{product?.width} см</p>
+                  </div>
+                  <div>
+                    <p className="opacity-60">Высота</p> <p>{product?.height} см</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="pt-4 flex gap-4">
+                {/* <ActiveFurniture product={product} /> */}
+                <button
                     onClick={() => {
                       removeProduct(product.id);
                     }}
@@ -532,12 +563,19 @@ function App() {
                     </span> 
                     */}
                   </button>
-                </div>
+                  <button className="btn btn-sm">
+                  <i className="bi bi-menu-button-wide-fill"></i>
+                  </button>
 
-                <p>{product.name}</p>
+                <button className="btn btn-sm flex-1">
+                  <i className="bi bi-cart"></i> В корзину
+                </button>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
+
+          {/* Card Design END */}
+        </div>
 
           {activeProducts.length === 0 ? (
             <div className="flex justify-center items-center w-full">
